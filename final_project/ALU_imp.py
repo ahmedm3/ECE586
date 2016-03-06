4f24475bef3526272b8cff8265d17bf95f4580cf
Ahmed Hashim Abdulkareem <ahmed.linkin@hotmail.com>
2016-03-05 21:28:06 -0800

    modified with delays

--- final_project/ALU_imp.py
+++ final_project/ALU_imp.py
@@ -6,7 +6,7 @@ ALU Implementation
 All rights reserved
 """
 
-from myhdl import enum, Signal, intbv, always
+from myhdl import enum, Signal, intbv, always, delay, instance, instances, traceSignals, Simulation
 
 ALU_OPS = enum(
     "ADD",
@@ -38,29 +38,65 @@ def ALU(A, B, result, status, clk, opcode, width=8):
     opcode --> specifies the operation (look at ALU_OPS)
     """
 
+    DELAY = 70
+
     #result = Signal(intbv(0)[width * 2 - 1:]) # make results to be a vector of size width
 
-    @always(clk.posedge)
+    #@always(clk.posedge)
+    @instance
     def alu():
-        if opcode.val == ALU_OPS.OR:
-            result.next = A | B
-        elif opcode.val == ALU_OPS.AND:
-            result.next = A & B
-        elif opcode.val == ALU_OPS.XOR:
-            result.next = A ^ B
-        elif opcode.val == ALU_OPS.ADD:
-            result.next = A + B
-        elif opcode.val == ALU_OPS.SUBTRACT:
-            result.next = A - B
-        elif opcode.val == ALU_OPS.MULTIPLY:
-            result.next = A * B
+        while True:
+            if opcode.val == ALU_OPS.OR:
+                #yield delay(DELAY)
+                yield clk.posedge
+                result.next = A | B
+            elif opcode.val == ALU_OPS.AND:
+                yield clk.posedge
+                result.next = A & B
+            elif opcode.val == ALU_OPS.XOR:
+                yield clk.posedge
+                result.next = A ^ B
+            elif opcode.val == ALU_OPS.ADD:
+                yield clk.posedge
+                result.next = A + B
+            elif opcode.val == ALU_OPS.SUBTRACT:
+                yield clk.posedge
+                result.next = A - B
+            elif opcode.val == ALU_OPS.MULTIPLY:
+                yield delay(DELAY)
+                result.next = A * B
 
-        # check if anything happened (Zero, negative, etc..)
-        if result.next < 0:
-            status.next = Signal(ALU_STATUSES.NEGATIVE)
-        elif result.next > 0:
-            status.next = Signal(ALU_STATUSES.POSITIVE)
-        elif result.next == 0:
-            status.next = Signal(ALU_STATUSES.ZERO)
+            # check if anything happened (Zero, negative, etc..)
+            if result.next < 0:
+                status.next = Signal(ALU_STATUSES.NEGATIVE)
+            elif result.next > 0:
+                status.next = Signal(ALU_STATUSES.POSITIVE)
+            elif result.next == 0:
+                status.next = Signal(ALU_STATUSES.ZERO)
 
     return alu
+
+if __name__ == "__main__":
+    WIDTH = 8    
+    def test_ALU():
+        
+        clk = Signal(bool(0))
+        A, B = [Signal(intbv(-(2**(WIDTH-1)))) for i in range(2)]
+        result = Signal(intbv(0))
+        status = Signal(ALU_STATUSES.NEGATIVE)
+        op = Signal(ALU_OPS.OR)
+        alu_inst = ALU(A, B, result, status, clk, op)
+
+        @always(delay(10))
+        def clkdriver():
+            clk.next = not clk
+
+        A.next = 5
+        B.next = 10
+
+        return instances()
+
+    tb = traceSignals(test_ALU)
+    sim = Simulation(tb)
+    sim.run(1000)
+
