5b7f5c02b22910c4a5301f1352560987548b5603
Ahmed Hashim Abdulkareem <ahmed.linkin@hotmail.com>
2016-03-10 02:12:58 -0800

    not implemented yet

---
+++ final_project/instruction_decoder.py
@@ -0,0 +1,16 @@
+"""
+Ahmed Abdulkareem
+02/10/2015
+Final Project
+Instruction Decoder Implementation
+All rights reserved
+"""
+
+def decode(instruction):
+    """
+    Instruction Format: 
+    decodes instruction
+    instruction --> input
+
+    returns: decoded instruction
+    """
