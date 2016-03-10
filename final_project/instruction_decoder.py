b2e6f65a2b5a471d976e97fda739b759c2fad368
Ahmed Hashim Abdulkareem <ahmed.linkin@hotmail.com>
2016-03-10 13:37:04 -0800

    decodes opcode

--- final_project/instruction_decoder.py
+++ final_project/instruction_decoder.py
@@ -6,11 +6,26 @@ Instruction Decoder Implementation
 All rights reserved
 """
 
+import config
+
 def decode(instruction):
     """
     Instruction Format: 
     decodes instruction
-    instruction --> input
+    instruction --> input: list of four elements (32 bits)
 
     returns: decoded instruction
     """
+
+    decoded = {}
+    opcode = instruction[0][0]
+    
+    if opcode in config.ISA:
+        decoded['OPCODE'] = config.ISA[opcode]
+
+    return decoded
+
+if __name__ == '__main__':
+
+    for i in range(14):
+        print(decode(['%s0' % hex(i)[2:], '00', '00', '00'])['OPCODE'])
