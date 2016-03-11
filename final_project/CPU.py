b0dc9393238c710034779b80e38cb44d2f3bdef5
Ahmed Hashim Abdulkareem <ahmed.linkin@hotmail.com>
2016-03-10 19:10:04 -0800

    arithmetic working

--- final_project/CPU.py
+++ final_project/CPU.py
@@ -12,25 +12,30 @@ import ALU
 import instruction_decoder
 import config
 
-STACK = []
-
+#instruction format
+#0001 1000 0000 0000 0000 0001 0000 0010
+#01101 000000000 000000000 000000000
+#0110 1000 0000 0000 0000 0010 0000 0010
+#0x18000202
+# opc   src1       src2       dest
 def CPU(args):
     
-    Memory.Mem('0x0', din = '0x00000000', we = True)
-    Memory.Mem('0x4', din = '0x10000000', we = True)
-    Memory.Mem('0x8', din = '0x30000000', we = True)
-    Memory.Mem('0xc', din = '0xD0000000', we = True)
+    Memory.Mem('0x0', din = '0x20000102', we = True)
+    Memory.Mem('0x4', din = '0x68000000', we = True)
 
     #while True:
     decoded = {'OPCODE': 'None'}
     while 'OPCODE' in decoded and decoded['OPCODE'] != 'HALT':
         instruc = fetch(hex(config.PC))  
         decoded = decode(instruc['DATA'])
-        print(decoded['OPCODE'])
+        print('INSTRUCTION: %s, SRC1: %s, SRC2: %s, DEST: %s' % (decoded['OPCODE'], int(decoded['SRC1'], 16), int(decoded['SRC2'], 16), int(decoded['DEST'], 16)))
+        execute(decoded)
         ALU.ALU(0, 0, 'ADD')
         config.PC += 4
     
-    print("CLOCK: %d" % config.GLOBAL_CLOCK)
+    print(config.REGISTER)
+    #print("CLOCK: %d" % (config.GLOBAL_CLOCK)
+    print("CLOCK: %d" % (config.GLOBAL_CLOCK/config.CLOCK_CYCLE))
 
 
 def fetch(addr):
@@ -45,7 +50,23 @@ def fetch(addr):
     return Memory.Mem(addr)
 
 def decode(ins):
+    """
+    this function decodes instruction
+    
+    returns: instruction decoded
+    """
     return instruction_decoder.decode(ins)
+
+def execute(decoded_ins):
+    """
+    function to excute given instruction
+    """
+    
+    opcode = decoded_ins['OPCODE']
+
+    # if opcode is arithmetic operation
+    if opcode == 'ADD' or opcode == 'SUB' or opcode == 'MULT' or opcode == 'XOR' or opcode == 'OR' or opcode == 'AND':
+        config.REGISTER[int(decoded_ins['DEST'], 16)] = ALU.ALU(config.REGISTER[int(decoded_ins['SRC1'], 16)], config.REGISTER[int(decoded_ins['SRC2'], 16)], opcode)['RESULT']
 
 
 # -------------------------------------
--- final_project/instruction_decoder.py
+++ final_project/instruction_decoder.py
@@ -19,11 +19,38 @@ def decode(instruction):
 
     decoded = {}
     opcode = instruction[0][0]
+    decoded_bin = bin(int(''.join(instruction), 16))[2:]
+
+    while len(decoded_bin) < 32:
+        decoded_bin = '0' + decoded_bin
+    
+    opcode = hex(int(decoded_bin[0:5], 2))[2:]
     
     if opcode in config.ISA:
         decoded['OPCODE'] = config.ISA[opcode]
+    
+    src1 = hex(int(decoded_bin[5:15], 2))[2:]
+    src2 = hex(int(decoded_bin[15:24], 2))[2:]
+    dest = hex(int(decoded_bin[24:32], 2))[2:]
+
+    #src1 = extend_to_32(src1)
+    #src2 = extend_to_32(src2)
+    #dest = extend_to_32(dest)
+    
+    decoded['SRC1'] = src1
+    decoded['SRC2'] = src2
+    decoded['DEST'] = dest
+    
+    config.GLOBAL_CLOCK += 2 # 2 clock cycles (because we have latency of 2 for our memroy)
 
     return decoded
+
+def extend_to_32(data):
+
+    while len(data) < 8:
+        data = '0' + data
+
+    return data
 
 if __name__ == '__main__':
 
